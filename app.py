927ab117bb209c0be2753acf567d4f9ae5461229
LimMengShin <62892957+LimMengShin@users.noreply.github.com>
2025-01-20 20:51:07 +0800

    Add "add question" feature

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask, render_template, g, request, jsonify, redirect
+from flask import Flask, render_template, g, request, jsonify, redirect, flash
 import sqlite3
 from random import randint
 import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, RadioField, TextAreaField, SelectField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "secretkey"
 
 database = "database.db"
 page_num = 0
+
+class AddQuestionForm(FlaskForm):
+    subject = SelectField("Subject", choices=["Computing", "Math", "Physics", "Chemistry", "Economics", "English"], validators=[DataRequired()])
+    question = TextAreaField("Question", validators=[DataRequired()])
+    option1 = StringField("Option 1", validators=[DataRequired()])
+    option2 = StringField("Option 2", validators=[DataRequired()])
+    option3 = StringField("Option 3", validators=[DataRequired()])
+    option4 = StringField("Option 4", validators=[DataRequired()])
+    correct_option = RadioField("Correct option", choices=["Option 1", "Option 2", "Option 3", "Option 4"], validators=[DataRequired()])
 
 def get_db():
     db = getattr(g, "_database", None)
@@ -20,6 +33,12 @@ def query_db(query, args=(), one=False):
     rv = cur.fetchall()
     cur.close()
     return (rv[0] if rv else None) if one else rv
+
+def insert_db(query, args=()):
+    conn = get_db()
+    conn.execute(query, args)
+    conn.commit()
+    conn.close()
 
 @app.teardown_appcontext
 def close_connection(exception):
@@ -67,9 +86,25 @@ def questions():
     print(questions_list)
     return render_template("questions.html", questions_list=questions_list)
 
-@app.route("/add_questions")
+@app.route("/add_questions", methods=["GET", "POST"])
 def add_questions():
-    pass
+    form = AddQuestionForm()
+    if form.validate_on_submit():
+        subject = request.form.get("subject")
+        question = request.form.get("question")
+        option1 = request.form.get("option1")
+        option2 = request.form.get("option2")
+        option3 = request.form.get("option3")
+        option4 = request.form.get("option4")
+        correct_option = request.form.get("correct_option")
+        try:
+            correct_option = "1234".index(correct_option[-1])+1
+            insert_db("INSERT INTO questions (subject, question, option1, option2, option3, option4, correct_option) VALUES (?, ?, ?, ?, ?, ?, ?);", (subject, question, option1, option2, option3, option4, correct_option))
+            flash("Question submitted!")
+        except ValueError:
+            pass
+        return redirect("/add_questions")
+    return render_template("add_questions.html", form=form)
 
 @app.route("/get_question")
 def get_question():
